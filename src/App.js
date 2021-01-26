22a16ad3743d5edb3323850e6569912626a290ab
squirrelzt <squirrelzt@126.com>
2021-01-26 20:23:38 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Luckysheet from './component/Luckysheet'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Luckysheet/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
