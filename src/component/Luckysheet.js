32a8ef41c0005119ee8f00e3f40e1393145abeb3
squirrelzt <squirrelzt@126.com>
2021-03-04 14:56:20 +0800

    axios查询实现通过接口初始化数据

--- src/component/Luckysheet.js
+++ src/component/Luckysheet.js
@@ -1,107 +1,112 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getInitDataAction } from './store/actionCreators';
+import axios from "axios";
 
 
 class Luckysheet extends React.Component {
 
     componentDidMount() {
         const { data } = this.props;
-        console.log('初始化------------------');
-        console.log(data);
-        // 配置项
-        const options = {
-            container: 'luckysheet', // 设定DOM容器的id
-            title: 'online-excel', // 设定表格名称
-            lang: 'zh', // 设定表格语言
-            allowUpdate: true,
-            hook: {
-                cellDragStop: function (cell, postion, sheetFile, ctx, event) {
-                    console.info(cell, postion, sheetFile, ctx, event);
+        let result;
+        axios.get('/api/initData.json').then((res) => {
+            result = res.data;
+            console.log('axios --------------------');
+            console.log(result);
+            // 配置项
+            const options = {
+                container: 'luckysheet', // 设定DOM容器的id
+                title: 'online-excel', // 设定表格名称
+                lang: 'zh', // 设定表格语言
+                allowUpdate: true,
+                hook: {
+                    cellDragStop: function (cell, postion, sheetFile, ctx, event) {
+                        console.info(cell, postion, sheetFile, ctx, event);
+                    },
+                    rowTitleCellRenderBefore:function(rowNum,postion,ctx){
+                        // console.log(rowNum);
+                    },
+                    rowTitleCellRenderAfter:function(rowNum,postion,ctx){
+                        // console.log(ctx);
+                    },
+                    columnTitleCellRenderBefore:function(columnAbc,postion,ctx){
+                        // console.log(columnAbc);
+                    },
+                    columnTitleCellRenderAfter:function(columnAbc,postion,ctx){
+                        // console.log(postion);
+                    },
+                    cellRenderBefore:function(cell,postion,sheetFile,ctx){
+                        // console.log(cell,postion,sheetFile,ctx);
+                    },
+                    cellRenderAfter:function(cell,postion,sheetFile,ctx){
+                        // console.log(postion);
+                    },
+                    cellMousedownBefore:function(cell,postion,sheetFile,ctx){
+                        // console.log(postion);
+                    },
+                    cellMousedown:function(cell,postion,sheetFile,ctx){
+                        // console.log(sheetFile);
+                    },
+                    sheetMousemove:function(cell,postion,sheetFile,moveState,ctx){
+                        // console.log(cell,postion,sheetFile,moveState,ctx);
+                    },
+                    sheetMouseup:function(cell,postion,sheetFile,moveState,ctx){
+                        // console.log(cell,postion,sheetFile,moveState,ctx);
+                    },
+                    cellAllRenderBefore:function(data,sheetFile,ctx){
+                        // console.info(data,sheetFile,ctx)
+                    },
+                    updated:function(operate){
+                        // console.info(operate)
+                    },
+                    cellUpdateBefore:function(r,c,value,isRefresh){
+                        // console.info('cellUpdateBefore',r,c,value,isRefresh)
+                    },
+                    cellUpdated:function(r,c,oldValue, newValue, isRefresh){
+                        console.info('cellUpdated',r,c,oldValue, newValue, isRefresh)
+                    },
+                    sheetActivate:function(index, isPivotInitial, isNewSheet){
+                        // console.info(index, isPivotInitial, isNewSheet)
+                    },
+                    rangeSelect:function(index, sheet){
+                        // console.info(index, sheet)
+                    },
+                    commentInsertBefore:function(r, c){
+                        // console.info(r, c)
+                    },
+                    commentInsertAfter:function(r, c, cell){
+                        // console.info(r, c, cell)
+                    },
+                    commentDeleteBefore:function(r, c, cell){
+                        // console.info(r, c, cell)
+                    },
+                    commentDeleteAfter:function(r, c, cell){
+                        // console.info(r, c, cell)
+                    },
+                    commentUpdateBefore:function(r, c, value){
+                        // console.info(r, c, value)
+                    },
+                    commentUpdateAfter:function(r, c, oldCell, newCell ){
+                        // console.info(r, c, oldCell, newCell)
+                    },
+                    cellEditBefore:function(range ){
+                        // console.info(range)
+                    },
+                    workbookCreateAfter:function(json){
+                        // console.info(json)
+                    },
+                    rangePasteBefore:function(range,data){
+                        // console.info('rangePasteBefore',range,data)
+                        // return false; //Can intercept paste
+                    },
                 },
-                rowTitleCellRenderBefore:function(rowNum,postion,ctx){
-                    // console.log(rowNum);
-                },
-                rowTitleCellRenderAfter:function(rowNum,postion,ctx){
-                    // console.log(ctx);
-                },
-                columnTitleCellRenderBefore:function(columnAbc,postion,ctx){
-                    // console.log(columnAbc);
-                },
-                columnTitleCellRenderAfter:function(columnAbc,postion,ctx){
-                    // console.log(postion);
-                },
-                cellRenderBefore:function(cell,postion,sheetFile,ctx){
-                    // console.log(cell,postion,sheetFile,ctx);
-                },
-                cellRenderAfter:function(cell,postion,sheetFile,ctx){
-                    // console.log(postion);
-                },
-                cellMousedownBefore:function(cell,postion,sheetFile,ctx){
-                    // console.log(postion);
-                },
-                cellMousedown:function(cell,postion,sheetFile,ctx){
-                    // console.log(sheetFile);
-                },
-                sheetMousemove:function(cell,postion,sheetFile,moveState,ctx){
-                    // console.log(cell,postion,sheetFile,moveState,ctx);
-                },
-                sheetMouseup:function(cell,postion,sheetFile,moveState,ctx){
-                    // console.log(cell,postion,sheetFile,moveState,ctx);
-                },
-                cellAllRenderBefore:function(data,sheetFile,ctx){
-                    // console.info(data,sheetFile,ctx)
-                },
-                updated:function(operate){
-                    // console.info(operate)
-                },
-                cellUpdateBefore:function(r,c,value,isRefresh){
-                    // console.info('cellUpdateBefore',r,c,value,isRefresh)
-                },
-                cellUpdated:function(r,c,oldValue, newValue, isRefresh){
-                    console.info('cellUpdated',r,c,oldValue, newValue, isRefresh)
-                },
-                sheetActivate:function(index, isPivotInitial, isNewSheet){
-                    // console.info(index, isPivotInitial, isNewSheet)
-                },
-                rangeSelect:function(index, sheet){
-                    // console.info(index, sheet)
-                },
-                commentInsertBefore:function(r, c){
-                    // console.info(r, c)
-                },
-                commentInsertAfter:function(r, c, cell){
-                    // console.info(r, c, cell)
-                },
-                commentDeleteBefore:function(r, c, cell){
-                    // console.info(r, c, cell)
-                },
-                commentDeleteAfter:function(r, c, cell){
-                    // console.info(r, c, cell)
-                },
-                commentUpdateBefore:function(r, c, value){
-                    // console.info(r, c, value)
-                },
-                commentUpdateAfter:function(r, c, oldCell, newCell ){
-                    // console.info(r, c, oldCell, newCell)
-                },
-                cellEditBefore:function(range ){
-                    // console.info(range)
-                },
-                workbookCreateAfter:function(json){
-                    // console.info(json)
-                },
-                rangePasteBefore:function(range,data){
-                    // console.info('rangePasteBefore',range,data)
-                    // return false; //Can intercept paste
-                },
-            },
-            data: data,
-            // 更多其他设置...
-        };
-        window.luckysheet.create(
-            options
-        );
+                data: result,
+                // 更多其他设置...
+            };
+            window.luckysheet.create(
+                options
+            );
+        });
     }
 
     render() {
